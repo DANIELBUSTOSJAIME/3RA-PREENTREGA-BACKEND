4d8c36c9d82fec3b81f08154e1abc498a299247a
DANIELBUSTOSJAIME <123820959+DANIELBUSTOSJAIME@users.noreply.github.com>
2023-12-17 00:45:02 -0300

    Desafio de logger

--- backend/src/config/loggers.js
+++ backend/src/config/loggers.js
@@ -1,57 +1,103 @@
 import winston from "winston"
 
-const customLevelOpt = {
+let logger;
+
+if (process.env.MODO === "development") {
+  const customLevelOpt = {
     levels: {
-        fatal: 0,
-        error: 1,
-        warning: 2,
-        info: 3,
-        debug: 4
+      fatal: 0,
+      error: 1,
+      warning: 2,
+      info: 3,
+      debug: 4
     },
     colors: {
-        fatal: 'red',
-        error: 'yellow',
-        warning: 'cyan',
-        info: 'blue',
-        debug: 'gray'
+      fatal: "red",
+      error: "yellow",
+      warning: "cyan",
+      info: "blue",
+      debug: "gray"
     }
-}
+  }
 
-const logger = winston.createLogger({
+  logger = winston.createLogger({
     levels: customLevelOpt.levels,
     transports: [
-        new winston.transports.File({
-            filename: './errors.html',
-            level: 'fatal',
-            format: winston.format.combine(winston.format.simple())
-        }),
-        new winston.transports.File({
-            filename: './errors.html',
-            level: 'error',
-            format: winston.format.combine(winston.format.simple())
-        }),
-        new winston.transports.File({
-            filename: './loggers.html',
-            level: 'warning',
-            format: winston.format.combine(winston.format.simple())
-        }),
-        new winston.transports.File({
-            filename: './loggers.html',
-            level: 'info',
-            format: winston.format.combine(winston.format.simple())
-        }),
-        new winston.transports.Console({
-            level: 'debug',
-            format: winston.format.combine(
-                winston.format.colorize({ colors: customLevelOpt.colors}),
-                winston.format.simple()
-            )
-        })
+      new winston.transports.File({
+        filename: './errors.html',
+        level: 'fatal',
+        format: winston.format.combine(winston.format.simple())
+      }),
+      new winston.transports.File({
+        filename: './errors.html',
+        level: 'error',
+        format: winston.format.combine(winston.format.simple())
+      }),
+      new winston.transports.File({
+        filename: './loggers.html',
+        level: 'warning',
+        format: winston.format.combine(winston.format.simple())
+      }),
+      new winston.transports.File({
+        filename: './loggers.html',
+        level: 'info',
+        format: winston.format.combine(winston.format.simple())
+      }),
+      new winston.transports.Console({
+        level: 'debug',
+        format: winston.format.combine(
+          winston.format.colorize({ colors: customLevelOpt.colors }),
+          winston.format.simple()
+        )
+      })
     ]
-})
+  })
+} else {
+  const customLevelOpt = {
+    levels: {
+      fatal: 0,
+      error: 1,
+      warning: 2,
+      info: 3,
+    },
+    colors: {
+      fatal: "red",
+      error: "yellow",
+      warning: "cyan",
+      info: "blue",
+    }
+  }
+
+  logger = winston.createLogger({
+    levels: customLevelOpt.levels,
+    transports: [
+      new winston.transports.File({
+        filename: './errors.html',
+        level: 'fatal',
+        format: winston.format.combine(winston.format.simple())
+      }),
+      new winston.transports.File({
+        filename: './errors.html',
+        level: 'error',
+        format: winston.format.combine(winston.format.simple())
+      }),
+      new winston.transports.File({
+        filename: './loggers.html',
+        level: 'warning',
+        format: winston.format.combine(winston.format.simple())
+      }),
+      new winston.transports.File({
+        filename: './loggers.html',
+        level: 'info',
+        format: winston.format.combine(winston.format.simple())
+      })
+    ]
+  })
+}
 
 export const addLogger = (req, res, next) => {
-    req.logger = logger,
-    req.logger.debug(`${req.method} es ${req.url} - ${new Date().toLocaleTimeString()}`)
-    next()
+  req.logger = logger,
+    req.logger.info(`${req.method} es ${req.url} - ${new Date().toLocaleTimeString()}`)
+  next()
 }
+
